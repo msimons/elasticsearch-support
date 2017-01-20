9bd024c83591c59a1283cfd93a9b66517b6102da
Jasper Huzen <feaster83@gmail.com>
2017-01-20 16:20:13 +0100

    Fix issue with fragmented jobs and fix job record removal system

--- src/main/java/org/xbib/elasticsearch/helper/client/AcknowledgeMetric.java
+++ src/main/java/org/xbib/elasticsearch/helper/client/AcknowledgeMetric.java
@@ -1,23 +1,21 @@
 package org.xbib.elasticsearch.helper.client;
 
 
-import org.elasticsearch.action.bulk.BulkItemResponse;
-
 import java.util.HashMap;
 import java.util.Map;
 
 public class AcknowledgeMetric {
-    private Map<DocumentIdentity,Boolean> results = new HashMap<>();
+    private Map<Long, Boolean> results = new HashMap<>();
 
-    public void addSucceeded(BulkItemResponse response) {
-        results.put(new DocumentIdentity(response),true);
+    public void addSucceeded(Long relatedJobId) {
+        results.put(relatedJobId, true);
     }
 
-    public void addFailed(BulkItemResponse response) {
-        results.put(new DocumentIdentity(response),false);
+    public void addFailed(Long relatedJobId) {
+        results.put(relatedJobId, false);
     }
 
-    public Map<DocumentIdentity, Boolean> getResult() {
+    public Map<Long, Boolean> getResult() {
         return results;
     }
 }
